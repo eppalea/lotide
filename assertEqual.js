fcfcdad65f131767535be006b29447f577634d54
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-02 01:06:11 +0000

    created a function that compares two values, and prints a msg as to whether they match or not

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑🛑🛑Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("✅✅✅Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Eppa Lea", "Eppa Lea");
+assertEqual("Cat", "Dog");
+assertEqual(27, 27);
+assertEqual(10, 100);
