f1ff2024932b9597a25317b4b2483d39b4c06888
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-04 04:25:52 +0000

    create a function that takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let genre = '';
+  for (const tvShowGenre in bestTVShowsByGenre) { //looping through each key in the obj
+    const showName = bestTVShowsByGenre[tvShowGenre]; //value is equal to the key of the obj
+    //console.log("Tv show genre: " + tvShowGenre + ", show name: " + showName);
+    if (value === showName) { //if the value parameter is the same as the showName from the for loop
+      genre = tvShowGenre; // the genre variable will be replaced with the tvShowGenre key
+    }
+  }
+  return genre;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
