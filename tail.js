328910b292ae0f5aa9874bcbdccc82a43869246c
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-08 21:29:30 +0000

    moved the test code to its own new file. refactored code

--- tail.js
+++ tail.js
@@ -6,28 +6,4 @@ const tail = function(array) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const animals = ["cat"];
-tail(animals);
-assertEqual(animals.length, 0);
-
-const birds = [];
-tail(birds);
-assertEqual(birds.length);
+module.exports = tail;
