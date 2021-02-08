e8713692d67eb19035e996999c29fdf117fc201c
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-08 23:26:07 +0000

    add mocha and chai based test code. This type of code uses the describe and it functions. removed old test code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,8 @@
-const assertEqual = require("../assertEqual");
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+describe("if two array are equal, return true", () => {
+  it('return true if [1, 2, 3] is equal to [1, 2, 3]', () => {
+      assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+  });
+});
