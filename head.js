03c9a4d803503bb56682196f0dbc8011e9b366ac
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-08 21:14:58 +0000

    refactored my code, by moving the assertEqual function to its own test file

--- head.js
+++ head.js
@@ -2,13 +2,4 @@ const head = function(array) {
   return array[0];
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
