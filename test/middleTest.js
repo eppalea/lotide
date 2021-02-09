4c391831e587a00ce3830f1e08c9a2e49cafbca4
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-09 01:33:39 +0000

    removed old test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,6 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
 describe("returns the middle index of an array. if array contains even num of elements, return middle 2 indices", () => {
   it('return [] if array is [1]', () => {
     assert.deepEqual(middle([]), []);
